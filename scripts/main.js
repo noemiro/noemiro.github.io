6924e3d32bb306d9af3aabf81e96cc0fee1a86d9
noemiro <59878764+noemiro@users.noreply.github.com>
2020-05-19 15:49:24 +0200

    j

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 //piece of code that does one or more actions
 //do not repeat yourself DRY
 
-var 
-textOverImage=document.querySelectorAll(".onClickTextOverImage div");
+var textOverImages = document.getElementsByClassName("onClickTextOverImage");
+var previousTextOverImage;
+for (var i = 0; i < textOverImages.length; i++) {
+  textOverImages[i].onclick = function() {
+    var classes = this.classList;
+    if (classes.contains("show")) {
+      classes.remove("show");
+    } else {
+      if (previousTextOverImage != null)
+        previousTextOverImage.classList.remove("show");
+      previousTextOverImage = this;
+      classes.add("show");
+    }
+  }
+}
 
+function stopPropagation(event){
+  event.stopPropagation();
+}
 
-for(var i=0;i<textOverImages.length;i++){
-    textOverImages[i].onclick=function(){
-        this.classList.toggle("show");
-
+//search bar//
+var searchbar = document.querySelector("#search");
+function slide(){
+    
+    if(searchbar.classList.contains("hide")){
+        searchbar.classList.remove("hide");
+    }else{
+         searchbar.classList.add("hide");
     }
 }
---
+++ scripts/search.js
@@ -0,0 +1,9 @@
+var searchbar = document.querySelector("#search");
+function slide(){
+    
+    if(searchbar.classList.contains("hide")){
+        searchbar.classList.remove("hide");
+    }else{
+         searchbar.classList.add("hide");
+    }
+}
