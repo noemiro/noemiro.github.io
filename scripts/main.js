885ea039d22b35bad73abc6c411763904bac46cf
noemiro <59878764+noemiro@users.noreply.github.com>
2020-05-19 10:10:07 +0200

    n

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 //piece of code that does one or more actions
 //do not repeat yourself DRY
 
+var 
+textOverImage=document.querySelectorAll(".onClickTextOverImage div");
+
+
+for(var i=0;i<textOverImages.length;i++){
+    textOverImages[i].onclick=function(){
+        this.classList.toggle("show");
+
+    }
+}
